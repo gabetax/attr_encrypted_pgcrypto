d75795dc49cd0c9bc90496d1bbec4ca2456726a5
Gabe Martin-Dempesy <gabe@mudbugmedia.com>
2013-01-14 15:27:30 -0600

    rspec :focus support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,4 +14,16 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'random'
+
+  # When setting boolean tags/metadata, allow an array shorthand instead of hash
+  #
+  # Example
+  #
+  #     it "wants my attention", :focus do
+  #
+  # https://www.relishapp.com/rspec/rspec-core/docs/metadata/user-defined-metadata
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+
+  # https://www.relishapp.com/rspec/rspec-core/docs/filtering/run-all-when-everything-filtered
+  config.run_all_when_everything_filtered = true
 end
